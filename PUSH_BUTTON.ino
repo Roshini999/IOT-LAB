f34800c42680759130c6a755f4b7c2a40f8ff22b
Roshini999 <103513659+Roshini999@users.noreply.github.com>
2022-04-22 20:00:11 +0530

    Add files via upload

---
+++ PUSH_BUTTON.ino
@@ -0,0 +1,15 @@
+int btn=2;
+int temp;
+void setup()
+   {
+pinMode(btn,INPUT);
+Serial.begin(9600);
+   }
+   
+void loop() {
+temp=digitalRead(btn);
+Serial.print("Value of Button:");
+Serial.println(temp);
+}
+  
+     
